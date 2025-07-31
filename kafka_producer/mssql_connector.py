2190c6d47f086eeb2eae2375f039ae262976d720
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-07-31 18:12:43 +0200

    add functionality in mssql_connector.py file

--- kafka_producer/mssql_connector.py
+++ kafka_producer/mssql_connector.py
@@ -11,9 +11,14 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(BASE_DIR,'..','config','config.yaml')
 config_path = os.path.abspath(config_path)
 
+CHECKPOINT_PATH = os.path.join(BASE_DIR,'checkpoints')
+CHECKPOINT_PATH = os.path.abspath(CHECKPOINT_PATH)
+os.makedirs(CHECKPOINT_PATH,exist_ok=True)
+
 class MSSQLConnector:
     def __init__(self,config_path = config_path):
 
+        self.conn = None
         # Load environment variables from .env
         load_dotenv()
 
@@ -38,6 +43,9 @@ class MSSQLConnector:
 
     def get_connection(self):
         '''Establish and return a database connection.'''
+        if self.conn:
+            return self.conn
+
         # Define connection string for MSSQL Server
         conn_str = (
             f'DRIVER={self.driver};'
@@ -48,10 +56,101 @@ class MSSQLConnector:
         )
 
         try:
-            conn = pyodbc.connect(conn_str)
+            self.conn = pyodbc.connect(conn_str)
             logging.info(f'Connected to server: {self.server}, database: {self.database}')
-            return conn
+            return self.conn
         except pyodbc.Error as err:
             logging.error(f'Connecion Error: {err}')
             return None
         
+    def read_last_fetched(self):
+        '''Read the last fetched timestamp from file, or return default.'''
+        path=os.path.join(CHECKPOINT_PATH, 'last_fetched.txt')
+        try:
+            if os.path.exists(path):
+                with open(path,'r') as f:
+                    timestamp = f.read().strip()
+                    logging.info(f'Last fetched timestamp read: {timestamp}')
+                    return timestamp
+            else:
+                default = '1900-01-01 00:00:00'
+                logging.info(f'Checkpoint not found. Using default: {default}')
+                return default
+        except Exception as e:
+            logging.error(f'Failed to read checkpoint: {e}')
+            return default
+        
+    def write_last_fetched(self,timestamp):
+        '''Write the latest timestamp to checkpoint file.'''
+        path=os.path.join(CHECKPOINT_PATH, 'last_fetched.txt')
+        try:
+            with open(path,'w') as f:
+                f.write(timestamp)
+                logging.info(f'Updated last fetched timestamp: {timestamp}')
+        except Exception as e:
+            logging.error(f'Failed to write checkpoint: {e}')
+
+    def fetch_incremental_data(self):
+        path=os.path.join(CHECKPOINT_PATH, 'last_fetched.txt')
+        conn = self.get_connection()
+        if conn is None:
+            return []
+        
+        cursor = conn.cursor()
+        last_fetched = self.read_last_fetched()
+        try:
+            query = '''
+                SELECT id,name,category,last_updated
+                FROM products
+                WHERE last_updated > ?
+                ORDER BY last_updated ASC
+            '''
+            cursor.execute(query,last_fetched)
+            rows = cursor.fetchall()
+
+            if rows:
+                latest_timestamp = max([row[-1] for row in rows]).strftime('%Y-%m-%d %H:%M:%S')
+                self.write_last_fetched(latest_timestamp)
+                return rows
+            else:
+                logging.info('No new records found.')
+                return []
+        
+        except pyodbc.Error as e:
+            logging.error(f'Query Failed: {e}')
+            return []
+        
+    def fetch_last_record(self):
+        '''Read the last record from the product table'''
+        
+        self.conn = self.conn or self.get_connection()
+        if not self.conn:
+            return []
+        
+        cursor = self.conn.cursor()
+
+        try:
+            query = '''
+                    SELECT 
+                        TOP 1 id,name,category,last_updated
+                    FROM products
+                    ORDER BY id DESC
+                '''
+            cursor.execute(query)
+            row = list(cursor.fetchall())
+            return row
+        except pyodbc.Error as e:
+            logging.error(f'Query failed: {e}')
+            return []
+        
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+            logging.info('Database connection closed.')
+        
+if __name__ == '__main__':
+    connector = MSSQLConnector()
+    rows = connector.fetch_incremental_data()
+    for row in rows:
+        print(row)
--- sqlScripts/insert_sample.py
+++ sqlScripts/insert_sample.py
@@ -12,12 +12,14 @@ def insert_sample_data(n):
 
     try:
         connector = MSSQLConnector()
-        conn = connector.get_connection()
-        conn.autocommit = True
-        cursor = conn.cursor()
+        connector.get_connection()
+        connector.conn.autocommit = True
+        cursor = connector.conn.cursor()
+        last_record = connector.fetch_last_record()
 
         products = []
-        base_time = datetime.datetime.now() - datetime.timedelta(days=30)
+        last_id = last_record[0][0] if last_record else 0
+        base_time = last_record[0][3] if last_record else datetime.datetime.now() - datetime.timedelta(days=90)
         total_seconds = 30*24*3600
         # Generate 500 random seconds and sort them for incremental timestamps
         random_seconds = sorted(random.randint(0,total_seconds) for _ in range(n+1))
@@ -26,8 +28,9 @@ def insert_sample_data(n):
             name = fake.name()
             category = random.choice(categories)
             price = round(random.uniform(250.0,6578.0),2)
-            last_updated = base_time + datetime.timedelta(seconds=random_seconds[i])
-            products.append((i,name,category,price,last_updated))
+            clean_last_updated = (base_time + datetime.timedelta(seconds=random_seconds[i])).replace(microsecond=0)
+            str_last_updated = clean_last_updated.strftime('%Y-%m-%d %H:%M:%S')
+            products.append((i + last_id,name,category,price,str_last_updated))
         
         insert_query = '''
                      INSERT INTO products(id,name,category,price,last_updated)
@@ -40,9 +43,9 @@ def insert_sample_data(n):
         print(f'Error: {err}')
 
     finally:
-        if conn:
+        if connector.conn:
             cursor.close()
-            conn.close()
+            connector.close_connection()
 
 # Inserting Samples
-insert_sample_data(500)
+insert_sample_data(5)
